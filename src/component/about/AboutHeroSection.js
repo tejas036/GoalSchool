681b48cc95b81ad19951240efc164fc472b5b526
tejas036 <tejaskhadke45@gmail.com>
2025-11-10 16:39:03 +0530

    now

--- src/component/about/AboutHeroSection.js
+++ src/component/about/AboutHeroSection.js
@@ -145,19 +145,19 @@
 
 
 
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 import bgImage6 from "../../assets/About/childrens.jpeg";
 
 const AboutHeroSection = () => {
   const images = [bgImage6];
-  const [currentIndex, setCurrentIndex] = useState(0);
+  // const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000); // Change image every 3 seconds
-    return () => clearInterval(interval);
-  }, [images.length]);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  //   }, 3000); // Change image every 3 seconds
+  //   return () => clearInterval(interval);
+  // }, [images.length]);
 
   return (
     <div className="relative">
